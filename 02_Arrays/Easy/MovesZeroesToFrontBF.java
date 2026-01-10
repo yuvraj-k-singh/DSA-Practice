c978e6ae7e043634542568368cb20cc2e1ba8097
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2026-01-10 16:57:55 +0530

    Moves Zeroes To Front BF

---
+++ 02_Arrays/Easy/MovesZeroesToFrontBF.java
@@ -0,0 +1,43 @@
+/*
+WAP: Moves Zeroes To Front BF
+Time Complexity: O(2n)
+Space Complexity: O(n)
+Date: 10-jan-2026
+*/
+
+public class MovesZeroesToFrontBF {
+    public static int[] movesZeroes(int[] arr) {
+        int[] temp = new int[arr.length];
+        int nonZeroes = arr.length - 1;
+
+        //storing nonZeroes to new array
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] != 0){
+                temp[nonZeroes--] = arr[i];
+            }
+        }
+
+        //copy back to original array
+        for(int i=0; i<arr.length; i++){
+            arr[i] = temp[i];
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0,1,0,3,12};
+
+        System.out.println("Array element before: ");
+        for(int x : arr){
+            System.out.print(x + " ");
+        }
+
+        movesZeroes(arr);
+
+        System.out.println();
+        System.out.println("\nArray element before: ");
+        for(int x : arr){
+            System.out.print(x + " ");
+        }
+    }
+}
