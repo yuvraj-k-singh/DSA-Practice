de5c043ce163f7a75fb32eb260cbf293ecb563dd
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2026-03-29 22:23:57 +0530

    Best Time To Buy and Sell

--- 02_Arrays/Easy/BestTimeToBuySell.java
+++ 02_Arrays/Easy/BestTimeToBuySell.java
@@ -1,17 +1,32 @@
-package Easy;
+/*
+WAP: Best Time To Buy and Sell
+Time Complexity: O(n)
+Space Complexity: O(1)
+Date: 29-mar-2026
+*/
 
 public class BestTimeToBuySell {
     public static void main(String[] args){
-        int[] arr = {7,1,5,3,6,4};
+        int[] arr = {7,6,4,3,1};
 
         int max = buySell(arr);
 
         System.out.println("Array elements: ");
         for(int x : arr) System.out.print(x+" ");
 
-        System.out.println("\nMaximum profit: "+max);
+        System.out.println();
+        System.out.print("\nMaximum profit: "+max);
     }
 
-    private static int buySell(int[] arr) {
+    public static int buySell(int[] arr) {
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        int profit = 0;
+
+        for(int x : arr){
+            min = Math.min(min, x);
+            profit = x - min;
+            max = Math.max(max, profit);
+        }
+        return max;
     }
 }
