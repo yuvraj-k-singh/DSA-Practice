d5b95ee958a156f380b638542708841dcafb3617
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2026-04-01 21:16:59 +0530

    Kth Pair Difference

---
+++ 02_Arrays/Medium/KthPairDifference.java
@@ -0,0 +1,31 @@
+/*
+WAP: GFG Kth Pair Difference
+Time Complexity: O(n)
+Space Complexity: O(n)
+Date: 01-apr-2026
+*/
+
+import java.util.HashSet;
+
+public class KthPairDifference {
+    public static void main(String[] args){
+        int[] arr = {1,3,1,5,4};
+        int target = 0;
+
+        int count = kthDifference(arr, target);
+
+        System.out.println("The total pair is: "+count);
+    }
+
+    private static int kthDifference(int[] arr, int target) {
+        HashSet<Integer> set = new HashSet<>();
+        int count = 0;
+
+        for(int x : arr){
+            int rem = Math.abs(target - x);
+            if(set.contains(rem)) count++;
+            set.add(x);
+        }
+        return count;
+    }
+}
