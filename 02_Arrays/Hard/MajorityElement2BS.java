1b8d23c6a8d765c0927714bfd7ee611f381dbd0f
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2026-06-28 11:50:11 +0530

    Majority Element2 BS

---
+++ 02_Arrays/Hard/MajorityElement2BS.java
@@ -0,0 +1,31 @@
+/*
+WAP: Majority Element2 BS
+Time Complexity: O(N^2)
+Space Complexity: O(1)
+Date: 28-june-2026
+*/
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MajorityElement2BS {
+    public static void main(String[] args) {
+        int[] nums = {2,1,1,3,1,4,5,6};
+
+        ArrayList<Integer> ans = majorityBS(nums);
+
+        System.out.print("Majority elements are: "+ans);
+    }
+
+    public static ArrayList<Integer> majorityBS(int[] nums){
+        ArrayList<Integer> ans = new ArrayList<>();
+        int n = (nums.length/3) + 1;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+            if(map.get(nums[i]) == n) ans.add(nums[i]);
+        }
+        return ans;
+    }
+}
