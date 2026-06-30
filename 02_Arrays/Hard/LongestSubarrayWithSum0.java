1d04e0b8e50d0aa5f26b3d7ece77ee2f38a47150
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2026-06-30 23:20:33 +0530

    Longest Subarray With Sum 0

---
+++ 02_Arrays/Hard/LongestSubarrayWithSum0.java
@@ -0,0 +1,32 @@
+/*
+WAP: Longest Subarray With Sum 0
+Time Complexity: O(N)
+Space Complexity: O(1)
+Date: 30-june-2026
+*/
+
+import java.util.HashMap;
+
+public class LongestSubarrayWithSum0 {
+    public static void main(String[] args) {
+        int[] nums = {15, -2, 2, -8, 1, 7, 10, 23};
+
+        int len = longestSubarray(nums);
+
+        System.out.println("Longest subarray length with sum 0 is: "+len);
+    }
+
+    public static int longestSubarray(int[] nums){
+        int n = nums.length;
+        int sum = 0, maxLen = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<n; i++){
+            sum += nums[i];
+            if(sum == 0) maxLen = i+1;
+            if(map.containsKey(sum)) maxLen = Math.max(maxLen, i-map.containsKey(sum));
+            if(!map.containsKey(sum)) map.put(sum, i);
+        }
+        return maxLen;
+    }
+}
