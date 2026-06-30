175d54e159d188fc6a34f72cffef68b42271efbf
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2026-06-30 23:21:36 +0530

    Longest Subarray With Sum 0

--- 02_Arrays/Hard/LongestSubarrayWithSum0.java
+++ 02_Arrays/Hard/LongestSubarrayWithSum0.java
@@ -24,7 +24,7 @@ public class LongestSubarrayWithSum0 {
         for(int i=0; i<n; i++){
             sum += nums[i];
             if(sum == 0) maxLen = i+1;
-            if(map.containsKey(sum)) maxLen = Math.max(maxLen, i-map.containsKey(sum));
+            if(map.containsKey(sum)) maxLen = Math.max(maxLen, i-map.get(sum));
             if(!map.containsKey(sum)) map.put(sum, i);
         }
         return maxLen;
