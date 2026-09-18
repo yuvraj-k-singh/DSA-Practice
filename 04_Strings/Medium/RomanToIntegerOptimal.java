5212f08b12fcb4afe64da6a1c119d6c56ebea573
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2026-09-18 22:26:32 +0530

    Roman To Integer Optimal

---
+++ 04_Strings/Medium/RomanToIntegerOptimal.java
@@ -0,0 +1,39 @@
+/*
+WAP: Roman To Integer Optimal
+Time Complexity: O(N)
+Space Complexity: O(1)
+Date: 18-sep-2026
+*/
+
+public class RomanToIntegerOptimal {
+    public static void main(String[] args){
+        String s = "VIII";
+
+        int val = romanToIntegerBF(s);
+        System.out.println("The value of "+s+" is: "+val);
+    }
+
+    public static int romanToIntegerBF(String s){
+        int ans = 0;
+
+        for(int i=0; i<s.length()-1; i++){
+            if(getValue(s.charAt(i)) < getValue(s.charAt(i+1))){
+                ans -= getValue(s.charAt(i));
+            }else ans += getValue(s.charAt(i));
+        }
+        return ans + getValue(s.charAt(s.length()-1));
+    }
+
+    private static int getValue(char ch){
+        switch(ch){
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+}
